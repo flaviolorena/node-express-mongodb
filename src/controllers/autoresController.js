0cf5122e59f05299b7e730fd20df32bdb8223a89
Flavio Lorena <flaviolorena.c@gmail.com>
2022-08-12 18:04:34 -0300

    listagem por id

--- src/controllers/autoresController.js
+++ src/controllers/autoresController.js
@@ -3,11 +3,11 @@ import autores from '../models/Autor.js';
 class AutorController {
   
 //find encontra todos os autores, o callback devolve os autores encontrados em json. há tratamento de erro como parametro
-  static listarAutores = (req,res) =>{
-    autores.find((err, autores) =>{
-      res.status(200).json(autores);
-    })
-  }
+  // static listarAutores = (req,res) =>{
+  //   autores.find((err, autores) =>{
+  //     res.status(200).json(autores);
+  //   })
+  // }
 
   //cara youtube q funciona
   // static listarAutores = async (req,res) =>{
@@ -18,22 +18,43 @@ class AutorController {
   
   //teste adaptação
   static listarAutores = async (req,res) =>{
-    const autoresBusca = await autores.find()
-    if(!autoresBusca) return res.sendStatus(400).json({'message': "Falha ao buscar autores"});
-    res.json(autoresBusca)
+    const autor = await autores.find()
+    if(!autor) return res.status(400).send({message: `${err.message} - Autor não encontrado`})
+    res.json(autor)
   }
 
-
-  static listarAutorPorId = (req,res) =>{
+  // static listarAutorPorId = (req,res) =>{
+  //   const id = req.params.id;
+  //   autores.findById(id, (err,autores) =>{
+  //     if(err){
+  //       res.status(400).send({message: `${err.message} - ID não encontrado`})
+  //     }else{
+  //       res.status(200).send(autores)
+  //     }
+  //   } )
+  // }  
+  
+  static listarAutorPorId = async (req,res) =>{
     const id = req.params.id;
-    autores.findById(id, (err,autores) =>{
-      if(err){
-        res.status(400).send({message: `${err.message} - ID não encontrado`})
-      }else{
-        res.status(200).send(autores)
-      }
-    } )
-  }
+    const autor = await autores.findById(id)
+    if(!autor){
+      //não esta exibindo a mensagem de erro
+      res.status(200).send({'message': "ID nao encontrado"})
+    }else{
+      res.status(200).send(autor)
+    }
+
+    // if(!autor){
+    //   // res.status(400).json({"message": "Sem autores"});
+    //   // console.log("nao encontrado")
+    //   res.send("nao encontrado")
+    //   console.log("nao deu")
+
+    // }else{
+    //   res.status(200).send(autor)
+    // }
+}  
+  
 
 // utiliza o schema para cadastrar autor, o metodo save para salvar no banco com o tratamento de erro. caso sucesso, retorna o status e converte em JSON 
   static cadastrarAutor = (req,res) =>{
